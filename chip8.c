88a3ac3226f79a2a26da6e38441f4c959b0befe7
Pritam Mondal <pritam987@gmail.com>
2026-07-17 10:30:58 +0530

    added 0XF istructions

--- chip8.c
+++ chip8.c
@@ -438,6 +438,34 @@ void print_debug_info(chip8_t *chip8){
 
             }
             break;
+
+        case 0x0F:
+            switch (chip8->inst.NN) {
+                case 0x0A:
+                    printf("Await until a key is pressed; Store key in V%X\n", chip8->inst.X);
+                    break;
+                case 0x1E:
+                    printf("I (0x%04X) += V%X (0x%02X) Result: (I): 0x%04X\n", chip8->I, chip8->inst.X, chip8->V[chip8->inst.X], chip8->I + chip8->V[chip8->inst.X]);
+                case 0x07:
+                    printf("Set V%X = delay timer value (0x%02X)\n", 
+                    chip8->inst.X, chip8->delay_timer);
+                    break;
+                case 0x15:
+                    printf("Set delay timer value = V%X (0x%02X)\n", chip8->inst.X, chip8->V[chip8->inst.X]);
+                    break;
+
+                case 0x18:
+                    printf("Set sound timer value = V%X (0x%02X)\n", chip8->inst.X, chip8->V[chip8->inst.X]);
+                    break;
+
+                case 0x29:
+                    printf("Set I to location in memory for character in V%X (0x%02X). Result(VX*5) = (0x%02X)\n",chip8->inst.X, chip8->V[chip8->inst.X], chip8->V[chip8->inst.X] * 5);
+                    break;
+
+                case 0x33:
+                    printf("Store BCD represntation of V%X (0x%02X) at memory from I (0x%04X)\n", chip8->inst.X, chip8->V[chip8->inst.X], chip8->I);
+                    break;
+            }
         default:
             printf("Unimplemented Opcode.\n");
             break;
@@ -620,23 +648,49 @@ void emulate_instructions(chip8_t *chip8, const config_t config){
             break;
         case 0x0F:
             switch (chip8->inst.NN) {
-            case 0x0A:
-                bool any_key_pressed = false;
-                for (uint8_t i = 0; i < sizeof chip8->keypad; i++){
-                    if (chip8->keypad[i]) {
-                        chip8->V[chip8->inst.X] = i;
-                        break;
+                case 0x0A:
+                    bool any_key_pressed = false;
+                    for (uint8_t i = 0; i < sizeof chip8->keypad; i++){
+                        if (chip8->keypad[i]) {
+                            chip8->V[chip8->inst.X] = i;
+                            break;
+                        }
                     }
-                }
-                if (!any_key_pressed) chip8->PC -= 2;
+                    if (!any_key_pressed) chip8->PC -= 2;
 
-                break;
-            
+                    break;
+                case 0x1E:
+                    chip8->I += chip8->V[chip8->inst.X];
+                    break;
+                    
+                case 0x07:
+                    chip8->V[chip8->inst.X] = chip8->delay_timer;
+                    break;
+
+                case 0x15:
+                     chip8->delay_timer = chip8->V[chip8->inst.X];            
+                    break;
+
+                case 0x18:
+                     chip8->sound_timer = chip8->V[chip8->inst.X];            
+                    break;
+
+                case 0x29:
+                    chip8->I = chip8->V[chip8->inst.X] * 5;
+                    break;
+
+                case 0x33:
+                    uint8_t bcd = chip8->V[chip8->inst.X];
+                    chip8->ram[chip8->I+2] = bcd % 10;
+                    bcd /= 10;
+                    chip8->ram[chip8->I+1] = bcd % 10;
+                    bcd /= 10;
+                    chip8->ram[chip8->I] = bcd;
+                    break;
             default:
                 break;
             }
 
-        default:
             break;
     }
 
